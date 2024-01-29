7dda80a3c65161077813030259db850e3e66fc43
praveensingh1999 <praveen969500@gmail.com>
2024-01-30 02:39:14 +0530

    add features

--- index.js
+++ index.js
@@ -12,14 +12,21 @@ io.on('connection', (socket) => {
     console.log('a user connected',socket.id);
     socket.on('disconnect', () => {
       console.log('user disconnected',socket.id);
-    })
+    });
 
-    socket.on('from_client',() => {
-      console.log("recived event from client");
-    })
-    setInterval(function f()  {
-      socket.emit('from_server');  
-    }, 3000);
+    // socket.on('from_client',() => {
+    //   console.log("recived event from client");
+    // })
+    // setInterval(function f()  {
+    //   socket.emit('from_server');  
+    // }, 3000);
+
+    socket.on('new_msg', (data) => {
+        io.emit('msg_rcvd',data);  // io.emit => it send all the socket connections.
+        // socket.emit('msg_rcvd', data);  // socket.io => it send to that socket only
+        // socket.broadcast.emit('msg_rcvd', data);  //apart from that socket( whose send the message) everyone received the message.
+
+    });
 });
 
 server.listen(3000,()=>{
--- public/script.js
+++ public/script.js
@@ -1,14 +1,36 @@
-console.log("welcome to socket");
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("welcome to socket");
 var socket = io();
-let btn = document.getElementById("btn");
-btn.addEventListener("click", () => {
-   socket.emit("from_client");
+// let btn = document.getElementById("btn");
+// btn.addEventListener("click", () => {
+//    socket.emit("from_client");
+// })
+
+// socket.on('from_server', () => {
+//     let div = document.getElementById("from_server");  //selecting the div
+//     let p = document.createElement("p"); // creating the p tag
+//     p.textContent = "Recieved an event from the server"; // assined value in p tag
+//     div.appendChild(p); // adding a p tag as child of div.
+
+// })
+
+let input = document.getElementById("chat_box");
+let msgList = document.getElementById("msg_list");
+let send = document.getElementById("send");
+
+send.addEventListener("click", () => {
+let msg = input.value;
+socket.emit('new_msg', {
+    message: msg 
+});
+input.value = "";   // after send the message empty the send box.
+});
+
+socket.on('msg_rcvd', (data) => {
+    let msg = document.createElement("li");
+    msg.textContent = data.message;
+    msgList.appendChild(msg);
+})
 })
 
-socket.on('from_server', () => {
-    let div = document.getElementById("from_server");  //selecting the div
-    let p = document.createElement("p"); // creating the p tag
-    p.textContent = "Recieved an event from the server"; // assined value in p tag
-    div.appendChild(p); // adding a p tag as child of div.
 
-})
